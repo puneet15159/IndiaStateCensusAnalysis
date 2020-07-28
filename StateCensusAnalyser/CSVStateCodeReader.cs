9d3b1f224168c604d90c9981e942e534b7ef61a7
puneet <puneet15159@iiitd.ac.in>
2020-07-28 22:56:36 +0530

    [puneet] Add: Method to Load state code CSV

---
+++ StateCensusAnalyser/CSVStateCodeReader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace StateCensusAnalyser
+{
+    class CSVStateCodeReader
+    {
+        public static int GetRecords(string filePath)
+        {
+            try
+            {
+                int count = 0;
+                string[] data = File.ReadAllLines(filePath);
+                IEnumerable<string> records = data;
+                foreach (var element in records)
+                {
+                    count++;
+                }
+
+                return count - 1;
+
+            }
+            catch (DirectoryNotFoundException e)
+            {
+
+                throw new StateCensusAnalyserException("You have entered a wrong directory path");
+            }
+            catch (FileNotFoundException)
+            {
+
+                throw new StateCensusAnalyserException("Name of the file is incorrect");
+            }
+        }
+
+
+    }
+}
